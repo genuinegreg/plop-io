91b20ede91dc373c09f204d7bcb205fa69afefaf
Grégoire Audoux <projects@gregoire-audoux.fr>
2022-11-21 12:12:58 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="container mx-auto p-4 font-sans font-normal leading-relaxed">
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-2xl">Plop.io</h1>
+    <section class="prose py-4">
+
+      <ContentDoc class="prose" path="/" />
+        
+
+      <!-- <h3 class="text-lg">Some stuff on the internet ⸮</h3>
+      <p>Some stupide stuff i build over teh years</p>
+      <ul class="list-inside list-disc">
+        <li><a class="underline" href="https://emoji.plop.io">Random emoji 😺🔓📬</a></li>
+        <li>Other stuff lost somewhere...</li>
+      </ul> -->
+    </section>
+  </main>
+</template>
+
+
+<style src="" />
---
+++ content/index.md
@@ -0,0 +1,7 @@
+
+### Some stuff on the internet ⸮
+
+Some stupid stuff i build over teh years
+
+- [Random emoji 😺🔓📬](https://emoji.plop.io)
+- Other stuff lost somewhere...
