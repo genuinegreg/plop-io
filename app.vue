2d7ee226c81dc4bff550c5bf0e27f3d315209deb
Grégoire Audoux <projects@gregoire-audoux.fr>
2022-11-22 14:04:23 +0100

    oupsy

--- app.vue
+++ app.vue
@@ -1,23 +1,18 @@
 <template>
   <main class="container mx-auto p-4 font-sans font-normal leading-relaxed">
 
-    <header>
+    <Header>
       <title>plop.io</title>
-    </header>
+    </Header>
 
     <!-- <NuxtWelcome /> -->
 
     <menu class="prose">
       <h1> <a href="/"> Plop.io </a></h1>
     </menu>
-    <content>
-
-      <NuxtPage class="prose">
-        <template #empty>
-          <h4>Document is empty</h4>
-        </template>
-      </NuxtPage>
-    </content>
+    <article>
+      <NuxtPage class="prose" />
+    </article>
   </main>
 </template>
 
